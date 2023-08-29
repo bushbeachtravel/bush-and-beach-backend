9917a6a9d77b96d09405aaf59bb93e18361593de
kennankole <kennankole@gmail.com>
2023-08-29 10:26:55 +0300

    Installing devise and creating users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  
+  controllers: {
+    sessions: 'api/v1/users/sessions',
+    registrations: 'api/v1/users/registrations'
+  }
+
+  get '/current_user', to: 'current_user#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/auth/google_oauth2/callback', to 'session#create'
 end
