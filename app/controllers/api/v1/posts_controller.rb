97469a594b81da81791977ea5ba21d7eb43b15be
kennankole <kennankole@gmail.com>
2023-09-11 12:55:21 +0300

    Setting comments and post controller endpoints. Refactoring routes to all anonymous users to view blog post list and detail

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::PostsController < ApplicationController
   # http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
   # before_action :set_post, only: %i[show update destroy]
   # before_action :authorize_post, only: %i[show update destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[update destroy create]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @posts = current_user.posts
+    @posts = Post.all
     render json: @posts
   end
 
@@ -16,9 +17,9 @@ class Api::V1::PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    # puts Rails.logger.debug(params.inspect)
+    puts Rails.logger.debug(params.inspect)
     if @post.save
-      handle_image_uploads(@post)
+      # handle_image_uploads(@post)
 
       render json: @post
     else
@@ -35,15 +36,15 @@ class Api::V1::PostsController < ApplicationController
 
   def update
     @post = current_user.posts.find(params[:id])
-
     if @post.update(post_update)
-      render json: { message: 'Post updated successfully.' }, status: 200
+      render json: @post
     else
       render json: { error: 'Unable to update post.' }, status: :bad_request
     end
   end
 
   def destroy
+    @post = Post.find(params[:id])
     if @post
       @post.destroy
       render json: { message: 'Post deleted successfully.' }, status: 200
@@ -55,7 +56,7 @@ class Api::V1::PostsController < ApplicationController
   private
 
   def post_update
-    params.require(:post).permit(:body)
+    params.require(:post).permit(body: {})
   end
 
   def post_params
@@ -66,24 +67,20 @@ class Api::V1::PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def authorize_post
-    authorize @post
-  end
+  # def handle_image_uploads(post)
+  #   editorjs_data = params[:post][:body]
 
-  def handle_image_uploads(post)
-    editorjs_data = params[:post][:body]
+  #   uploaded_images = []
 
-    uploaded_images = []
+  #   editorjs_data.each do |block|
+  #     if block['type'] == 'image'
+  #       image_data = block['data']
+  #       image_url = image_data['file']['url']
 
-    editorjs_data.each do |block|
-      if block['type'] == 'image'
-        image_data = block['data']
-        image_url = image_data['file']['url']
-
-        image = post.images.create(url: image_url)
-        uploaded_images << image
-      end
-    end
-    uploaded_images
-  end
+  #       image = post.images.create(url: image_url)
+  #       uploaded_images << image
+  #     end
+  #   end
+  #   uploaded_images
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :posts do
+        resources :posts, only: %i[create update destroy] do
           resources :likes, :comments
         end
       end
-      resources :posts do
+      resources :posts, only: %i[show index] do
         resources :likes, :comments
       end
     end
